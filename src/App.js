4e1bd2026cd2aed64a97d5afdd65dd37cf6a9974
Kohmei Kadoya <ajis.kkadoya@gmail.com>
2021-01-23 22:27:48 +0900

    Some sample react code

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Just to give you some basic html looking code</h1>
+      also note css styling is applied
+      <h5>Occasionally auto-refresh breaks! Just force refresh with Control+Shift+R</h5>
+
+
     </div>
   );
 }
 
 export default App;
+
+// <div className="RedBox">
+//   <ul>
+//     <li> Item 1</li>
+//     <li> Item 2</li>
+//     <li> Item 4</li>
+//   </ul>
+// </div>
+// <div>
+//   <ul className="BlueBox">
+//     <li className="Left"> Left</li>
+//     <li className="Right"> Right</li>
+//   </ul>
+// </div>
